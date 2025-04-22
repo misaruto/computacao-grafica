12882aa66b1b832676f340cbac5fc22f9e20fdf2
Misael Guilhardes de Freitas <misaelg.freitas2000@gmail.com>
2025-04-22 13:15:29 -0300

    Criando componente de fabrica de esferas

--- atv_1/src/components/customSphere.js
+++ atv_1/src/components/customSphere.js
@@ -1,9 +1,9 @@
 import * as THREE from 'three';
 const loader = new THREE.TextureLoader();
 
-export const fabricSphere = (textureName) => {
-  const texture = loader.load(`../public/assets/textures/${textureName}`);
-  const geometry = new THREE.SphereGeometry();
+export const fabricSphere = (textureName, radius = undefined) => {
+  const texture = loader.load(`/textures/${textureName}`);
+  const geometry = new THREE.SphereGeometry(radius);
   const material = new THREE.MeshBasicMaterial({
     map: texture,
   });
